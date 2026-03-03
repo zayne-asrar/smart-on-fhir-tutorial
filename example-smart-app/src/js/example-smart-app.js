e241470826d88f47d5c6f83422741930013150f9
zayne-asrar <zainasrar_17@yahoo.com>
2026-03-03 19:13:06 +0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,9 +15,12 @@
                     type: 'Observation',
                     query: {
                       code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        $or: ['http://loinc.org|8302-2', 
+                              //'http://loinc.org|8462-4',
+                              //'http://loinc.org|8480-6', 
+                              'http://loinc.org|2085-9',
+                              'http://loinc.org|2089-1', 
+                              'http://loinc.org|85354-9']
                       }
                     }
                   });
@@ -37,8 +40,8 @@
           }
 
           var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var systolicbp = getBloodPressureValue(byCodes('85354-9'),'8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
