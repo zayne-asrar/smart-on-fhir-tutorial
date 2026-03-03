1036c6bc025c6532bf18bd69fd779240bd5cc0ec
zayne-asrar <zainasrar_17@yahoo.com>
2026-03-03 17:56:43 +0400

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -17188,7 +17188,8 @@ BBClient.ready = function(input, callback, errback){
       // Check if 2 minutes from access token expiration timestamp
       var tokenResponse = getPreviousToken();
       var payloadCheck = jwt.decode(tokenResponse.access_token);
-      var nearExpTime = Math.floor(Date.now() / 1000) >= (payloadCheck['exp'] - 120);
+      //var nearExpTime = Math.floor(Date.now() / 1000) >= (payloadCheck['exp'] - 120);
+	  var nearExpTime = Math.floor(Date.now() / 1000) >= (tokenResponse.expires_in - 120);
 
       if (tokenResponse.refresh_token
         && tokenResponse.scope.indexOf('online_access') > -1
