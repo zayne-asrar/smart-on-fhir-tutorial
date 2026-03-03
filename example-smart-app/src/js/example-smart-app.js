9325ec9f2c540491c043a338a1ea4d63396875b8
zayne-asrar <zainasrar_17@yahoo.com>
2026-03-03 19:30:01 +0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -20,7 +20,8 @@
                               //'http://loinc.org|8480-6', 
                               'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 
-                              'http://loinc.org|85354-9']
+                              'http://loinc.org|85354-9',
+                              'http://loinc.org|8310-5']
                       }
                     }
                   });
@@ -44,6 +45,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var temperature = byCodes('8310-5');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -62,6 +64,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.temperature = getQuantityValueAndUnit(temperature[0]);
 
           ret.resolve(p);
         });
@@ -86,6 +89,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      temperature: {value: ''}
     };
   }
 
